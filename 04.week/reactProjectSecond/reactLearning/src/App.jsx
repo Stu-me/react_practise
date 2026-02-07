47e43f494a4a7f5f514a5b3670dff8bf098ea139
Stu-me <stusundram@gmail.com>
2026-02-07 18:38:12 +0530

    some old work done plus todo app started

--- 04.week/reactProjectSecond/reactLearning/src/App.jsx
+++ 04.week/reactProjectSecond/reactLearning/src/App.jsx
@@ -1,33 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+
+function Button(props){
+    function clickHandler(){
+      props.fnCnt(props.count+1)
+    }
+    return(
+      <>
+      <p className='text-2xl bg-green-300'> So babu saheb we made this button for you </p>
+      <button onClick={clickHandler} className='border-2 px-5 m-3 rounded-sm bg-amber-200 text-[30px]' >{props.count}</button>
+      </>
+      
+    )
+  }
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='text-2xl text-center bg-[#f8b771]'>
+        tailwind connected
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button count={count} fnCnt={setCount}></Button>
     </>
   )
 }
---
+++ 05.week/frontend/src/assets/asset.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const asset = () => {
+  return (
+    <div>asset</div>
+  )
+}
