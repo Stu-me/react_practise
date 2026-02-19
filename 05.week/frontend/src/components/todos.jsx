dc3baeb0039b5702c85f9b3f15c5e743637cff37
Stu-me <stusundram@gmail.com>
2026-02-19 16:34:32 +0530

    basic frontend created

---
+++ 05.week/frontend/src/components/todos.jsx
@@ -0,0 +1,8 @@
+
+export function Todos(){
+    return <div>
+        <h1> Go to flowstate</h1>
+        <h2> Remove distractions and put work ethic</h2>
+        <button> Mark as completed</button>
+    </div>
+}
